3d4974fc33f96ff97022861ad0787fcee521b497
Nia <niaespinal@outlook.com>
2025-06-10 22:45:57 +0200

    first commit

---
+++ src/main/java/com/FemCoders/Lean/Mean/Text/Machine/LeanMeanTextMachineApplication.java
@@ -0,0 +1,13 @@
+package com.FemCoders.Lean.Mean.Text.Machine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeanMeanTextMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeanMeanTextMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FemCoders/Lean/Mean/Text/Machine/LeanMeanTextMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FemCoders.Lean.Mean.Text.Machine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeanMeanTextMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
