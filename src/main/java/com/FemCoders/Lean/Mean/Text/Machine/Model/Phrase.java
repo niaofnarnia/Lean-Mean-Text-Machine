440dd319b3e99fc9844f494366b05f0bb4638bb8
Nia <niaespinal@outlook.com>
2025-06-11 12:56:16 +0200

    Create Model: Phrase

---
+++ src/main/java/com/FemCoders/Lean/Mean/Text/Machine/Model/Phrase.java
@@ -0,0 +1,49 @@
+package com.FemCoders.Lean.Mean.Text.Machine.Model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Phrase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String text;
+    private String author;
+
+    public Phrase() {
+    }
+
+    public Phrase(Long id, String text, String author) {
+        this.id = id;
+        this.text = text;
+        this.author = author;
+    }
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
