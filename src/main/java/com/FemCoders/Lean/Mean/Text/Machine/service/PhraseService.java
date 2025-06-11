9d7d6106bd9962757e643d18163651b25e0dfa2b
Nia <niaespinal@outlook.com>
2025-06-12 00:10:27 +0200

    Add PhraseService

---
+++ src/main/java/com/FemCoders/Lean/Mean/Text/Machine/service/PhraseService.java
@@ -0,0 +1,14 @@
+package com.FemCoders.Lean.Mean.Text.Machine.service;
+
+import com.FemCoders.Lean.Mean.Text.Machine.Model.Phrase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PhraseService {
+    List<Phrase> getAllPhrases();
+    Optional<Phrase> getPhraseById(Long id);
+    Phrase savePhrase(Phrase phrase);
+    Phrase updatePhrase(Long id, Phrase phrase);
+    void deletePhrase(Long id);
+}
